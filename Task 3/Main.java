3fe36de874863ccd13f1c11ea5564e4a8b83ae2d
Aleks1075 <buha_5@hotmail.com>
2022-03-01 15:19:34 +0100

    DS

---
+++ Task 3/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] myArray = { 1, 6423, 64, 6, 3, 123, 61, 6, 36, 1236, 347, 3, 1326, 246, 4 };
+
+        Arrays.sort(myArray);
+
+        System.out.println("New in order myArray is " + Arrays.toString(myArray));
+
+
+    }
+}
